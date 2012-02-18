c6c52dbd31fbbf0f559cb98d8625884dc2170b18
Juan Pablo Escalona G <irken.cs@gmail.com>
2012-02-17 22:12:33 -0300

    Muchas cosas agregadas
    Se agrego un sistema de historial, un botón de reseteo para volver a la
    configuración inicial. Se agregaron imágenes para las pintas de las
    cartas.

--- js/scripts.js
+++ js/scripts.js
@@ -12,22 +12,27 @@ fill_rules = function(reglas) {
 	}
 };
 
+generar_mazo = function() {
+	mazo = new Array();
+	
+	for (var i = 0; i < 4; i++)
+		for(var carta in cartas)
+			mazo.push(cartas[carta] + '-<img src="images/'+i+'.png" alt="carta" />');
+	
+	total = mazo.length;
+	return shuffle(mazo);
+};
+
 /* Generar Mazo */
 var cartas = ['as', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'diez', 'jota', 'quina', 'kaiser'];
 var reglas = ['Regla', 'Toma 2', 'Tome 3', 'Toma 4', 'Da 2', 'Da 3', 'Da 4', 'Historia', 'Pulgar', 'Cascada', 'Cultura', 'Rima', 'Shot'];
-var icons = ['As', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-var mazo = new Array();
-
-for (var i = 0; i < 4; i++)
-	for(var carta in cartas)
-		mazo.push(cartas[carta]);
-
-mazo = shuffle(mazo);
-var total = mazo.length;
-var mouse_is_inside = false;
+var icons  = ['As', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+var mazo   = null;
+var total  = 0;
 
 $(function() {
 	fill_rules(reglas);
+	mazo = generar_mazo();
 	
 	$('#config-button').click(function() {
 		$('#config').animate({width: 'toggle'});
@@ -36,13 +41,20 @@ $(function() {
 	$('#count-keep').html('Van: ' + (total - mazo.length) + ' - Quedan: ' + mazo.length);
 	
 	$('#sacar-carta').click(function() {
-		if(mazo.length > 0) {
-			var tmp = mazo.pop();
-			$('#juego li').html(icons[cartas.indexOf(tmp)]+': ' + $('#'+tmp).val());
-		}else {
+		if(mazo.length > 0) { // cards still in the deck
+			var tmp = mazo.pop().split('-');
+			var cardNumb = tmp[0]; /* Obtain Card Number (ej as, 2, 3, 4, 5, ..., Q, K) */
+			var image = tmp[1]; /* Obtain Card suits (pick, clubs, etc...) <- this loads the correct image */
+			
+			$('#juego li').html(icons[cartas.indexOf(cardNumb)] + image + ' : ' + $('#'+cardNumb).val());
+			                /* (get the card number    ) + image + ' : ' + Current value on the config */
+			
+			$('#jugadas').append('<li>'+ icons[cartas.indexOf(cardNumb)] + image + ' : ' + $('#'+cardNumb).val() + '</li>');
+		}else { // end of game
 			$('#juego li').html('fin');
 		}
-		if (total - mazo.length >= 0)
+		
+		if (total - mazo.length >= 0) // is game not over yet, update count of cards.
 			$('#count-keep').html('Van: ' + (total - mazo.length) + ' - Quedan: ' + mazo.length);
 			
 		return 0;
@@ -51,11 +63,21 @@ $(function() {
 	$('#save-config').click(function() {
 		var saveus = new Array();
 		
-		for (var carta in cartas) {
+		for (var carta in cartas)
 			saveus.push($('#'+cartas[carta]).val());
-		}
+		
 		$.cookie('cuartorey', saveus.join('-'));
 		$('#config').animate({width: 'toggle'});
 		return 0;
+	});
+	
+	$('#reset-config').click(function() {
+		$.cookie('cuartorey', null);
+		fill_rules(reglas);
+		$('#config').animate({width: 'toggle'});
+	});
+	
+	$('#historial-button').click(function() {
+		$('#historial').animate({height: 'toggle'});
 	});
 });
